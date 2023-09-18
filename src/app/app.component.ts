9790d679707275eb67bc68d230eb03a87ba1d647
Idan <idanco77@gmail.com>
2023-09-19 00:52:05 +0300

    adding introduction message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+declare const swal: any;
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {
+  }
 
+  ngOnInit() {
+    swal.fire({
+      title: '<strong>ברוכים הבאים!</strong>',
+      icon: 'info',
+      html:
+          'יש לגלות שם משפחה של שחקן מליגת העל שמכיל בדיוק 5 אותיות' +
+          ' עונת 2023-2024 ',
+      showCloseButton: true,
+      focusConfirm: false,
+      confirmButtonText:
+          '<i class="fa fa-thumbs-up"></i> צא לדרך!',
+      confirmButtonAriaLabel: 'Thumbs up, great!',
+      timer: 8000,
+    })
+
+  }
 }
