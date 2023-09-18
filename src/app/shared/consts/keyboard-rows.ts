4195af3df5b28b4e936914e46c3758ca11230b90
Idan <idanco77@gmail.com>
2023-09-18 23:48:44 +0300

    cancel local storage- make json

--- src/app/shared/consts/keyboard-rows.ts
+++
@@ -1,3 +0,0 @@
-export const FIRST_KEYBOARD_ROW = 0;
-export const SECOND_KEYBOARD_ROW = 1;
-export const THIRD_KEYBOARD_ROW = 2;
--- src/app/shared/services/helper.service.ts
+++
@@ -1,7 +0,0 @@
-export const mapPlayerDetails = (inputArray: string[]): string[] => {
-    return inputArray
-        .map((player: string) => player)
-        .filter((lastName: string, index: number, currentVal: string[]): boolean =>
-            currentVal.indexOf(lastName) === index
-        );
-}
